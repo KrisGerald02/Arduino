5f46b16b553f2635dfcda9461d7fc1c14153f6df
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2025-03-22 13:33:52 -0600

    Update sketch_encenderL13.ino

--- Modulo 1: Programando con Arduino/Codigos/sketch_encenderL13.ino
+++ Modulo 1: Programando con Arduino/Codigos/sketch_encenderL13.ino
@@ -1,1 +1,14 @@
+void setup() {
+  //Declaracion de Variables
+  //Aqui se trabaja que pin es de entrada/salida
+  pinMode(13, OUTPUT); //Salida
+}
 
+void loop() {
+  // Formamos Bucles
+  digitalWrite(13, HIGH); //High = Voltaje 5
+  delay(2000); //Se mantiene encendido 2000 milisegundos = 2 segundos
+  digitalWrite(13, LOW); //Low= Apagar
+  delay(2000); //1000 milisegundos = 1 segundos
+  
+}
